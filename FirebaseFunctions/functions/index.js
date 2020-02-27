584e021031a30cff12112273484fd6d829955fbe
Stephen M Crofton <smcrofton@hotmail.com>
2020-02-26 21:08:02 -0500

    Added Firebase Functionality
    Split code into two seperate folders:
    1. AgileBot; discord bot code
    2. FirebaseFunctions; firebase functions dealing with firestore database

---
+++ FirebaseFunctions/functions/index.js
@@ -0,0 +1,31 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const firebase = require("firebase");
+const serviceAccount = require("./trello-webhhok-firebase-adminsdk-j1z1c-c2b447b35b.json");
+
+const config = {
+    apiKey: "AIzaSyDg2byr8D7-EGHbG7_xZ9YkYPWOgDp0PhA",
+    authDomain: "trello-webhhok.firebaseapp.com",
+    databaseURL: "https://trello-webhhok.firebaseio.com",
+    projectId: "trello-webhhok",
+}
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://trello-webhhok.firebaseio.com"
+});
+
+firebase.initializeApp(config);
+
+const db = admin.firestore();
+const auth = firebase.auth();
+const fs = require('fs');
+
+exports.webhookTest = functions.https.onRequest((request, response) => {
+
+    console.log(request.body);
+    db.collection('trelloUpdateTest').add(request.body);
+
+    return response.status(200).send("if there is a god, help me now");
+
+});
---
+++ FirebaseFunctions/webhook.js
@@ -0,0 +1,14 @@
+const axios = require('axios')
+
+axios.post('https://api.trello.com/1/tokens/7038d4016f578c077da4b282d74a8aad0aa8cb068d9bd2b364a22e853384d453/webhooks/?key=f5f7b5f6456619c81fd348f7b69d4e08', {
+  description: "testing webhook",
+  callbackURL: "https://us-central1-trello-webhhok.cloudfunctions.net/webhookTest",
+  idModel: "5e502aed52e0dd6109a1f605",
+})
+.then((res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+  console.log(res)
+})
+.catch((error) => {
+  console.error(error)
+});
